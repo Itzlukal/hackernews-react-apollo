f92f27710ca635efecf9e5b0bf1cfcd9c8cdbaa9
LukaL <Lukalursmanashvili398@gmail.com>
2023-11-17 12:11:06 +0400

    implemeneted Authentication

--- src/index.js
+++ src/index.js
@@ -3,24 +3,32 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
-
-
-// 1
+import { setContext } from '@apollo/client/link/context';
 import {
   ApolloProvider,
   ApolloClient,
   createHttpLink,
   InMemoryCache
 } from '@apollo/client';
+import { AUTH_TOKEN } from './constants';
 
-// 2
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
-// 3
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
