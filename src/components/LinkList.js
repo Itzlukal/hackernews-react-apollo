719a15eba1f5da1ba5bc684511ea3e771b00797b
LukaL <Lukalursmanashvili398@gmail.com>
2023-11-17 15:41:22 +0400

    Realtime updates with subscriptions

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,6 +2,54 @@ import React from 'react';
 import Link from './Link';
 import { useQuery, gql } from '@apollo/client';
 
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
 export const FEED_QUERY = gql`
 {
   feed {
@@ -29,20 +77,50 @@ export const FEED_QUERY = gql`
 
 
 const LinkList = () => {
-  const { data } = useQuery(FEED_QUERY);
+  const {
+    data,
+    loading,
+    error,
+    subscribeToMore
+  } = useQuery(FEED_QUERY);
+
+  subscribeToMore({
+    document: NEW_VOTES_SUBSCRIPTION
+  });
+
+  subscribeToMore({
+    document: NEW_LINKS_SUBSCRIPTION,
+    updateQuery: (prev, { subscriptionData }) => {
+      if (!subscriptionData.data) return prev;
+      const newLink = subscriptionData.data.newLink;
+      const exists = prev.feed.links.find(
+        ({ id }) => id === newLink.id
+      );
+      if (exists) return prev;
   
-  return (
-    <div>
+      return Object.assign({}, prev, {
+        feed: {
+          links: [newLink, ...prev.feed.links],
+          count: prev.feed.links.length + 1,
+          __typename: prev.feed.__typename
+        }
+      });
+    }
+  });
+
+
+    return (
+      <div>
       {data && (
         <>
-          {data.feed.links.map((link, index) => (
-            <Link key={link.id} link={link} index={index} />
-            ))}
-        </>
-      )}
-      </div>
-  );
-  };
+        {data.feed.links.map((link, index) => (
+          <Link key={link.id} link={link} index={index} />
+          ))}
+          </>
+          )},
+          </div>
+          );
+        };
 
 
 export default LinkList;
